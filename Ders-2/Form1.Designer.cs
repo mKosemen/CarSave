9f0366947312865f7fc0aa4ebdcb77d94df5bf28
mKosemen <103363649+mKosemen@users.noreply.github.com>
2022-07-01 10:15:02 +0300

    CarSave

--- Ders-2/Form1.Designer.cs
+++ Ders-2/Form1.Designer.cs
@@ -29,23 +29,23 @@
         private void InitializeComponent()
         {
             this.gbxCreateCar = new System.Windows.Forms.GroupBox();
-            this.grpUpdate = new System.Windows.Forms.GroupBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.lblModel = new System.Windows.Forms.Label();
-            this.lblPrice = new System.Windows.Forms.Label();
-            this.lblUModel = new System.Windows.Forms.Label();
-            this.lblSearchId = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.btnSave = new System.Windows.Forms.Button();
-            this.btnFind = new System.Windows.Forms.Button();
-            this.btnUpdate = new System.Windows.Forms.Button();
-            this.txtModel = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblMarka = new System.Windows.Forms.Label();
             this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblModel = new System.Windows.Forms.Label();
+            this.grpUpdate = new System.Windows.Forms.GroupBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.txtUModel = new System.Windows.Forms.TextBox();
             this.txtUPrice = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.lblMarka = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblUModel = new System.Windows.Forms.Label();
+            this.lblSearchId = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.gbxCreateCar.SuspendLayout();
             this.grpUpdate.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -67,6 +67,63 @@
             this.gbxCreateCar.TabStop = false;
             this.gbxCreateCar.Text = "Create Car";
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // lblMarka
+            // 
+            this.lblMarka.AutoSize = true;
+            this.lblMarka.Location = new System.Drawing.Point(20, 60);
+            this.lblMarka.Name = "lblMarka";
+            this.lblMarka.Size = new System.Drawing.Size(37, 13);
+            this.lblMarka.TabIndex = 5;
+            this.lblMarka.Text = "Marka";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(85, 83);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPrice.TabIndex = 4;
+            // 
+            // txtModel
+            // 
+            this.txtModel.Location = new System.Drawing.Point(85, 31);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(100, 20);
+            this.txtModel.TabIndex = 3;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(85, 109);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(87, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(20, 90);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(31, 13);
+            this.lblPrice.TabIndex = 1;
+            this.lblPrice.Text = "Price";
+            // 
+            // lblModel
+            // 
+            this.lblModel.AutoSize = true;
+            this.lblModel.Location = new System.Drawing.Point(20, 34);
+            this.lblModel.Name = "lblModel";
+            this.lblModel.Size = new System.Drawing.Size(36, 13);
+            this.lblModel.TabIndex = 0;
+            this.lblModel.Text = "Model";
+            // 
             // grpUpdate
             // 
             this.grpUpdate.Controls.Add(this.textBox3);
@@ -83,100 +140,6 @@
             this.grpUpdate.TabIndex = 1;
             this.grpUpdate.TabStop = false;
             this.grpUpdate.Text = "Update";
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(23, 223);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(467, 187);
-            this.dataGridView1.TabIndex = 2;
-            // 
-            // lblModel
-            // 
-            this.lblModel.AutoSize = true;
-            this.lblModel.Location = new System.Drawing.Point(20, 34);
-            this.lblModel.Name = "lblModel";
-            this.lblModel.Size = new System.Drawing.Size(36, 13);
-            this.lblModel.TabIndex = 0;
-            this.lblModel.Text = "Model";
-            // 
-            // lblPrice
-            // 
-            this.lblPrice.AutoSize = true;
-            this.lblPrice.Location = new System.Drawing.Point(20, 90);
-            this.lblPrice.Name = "lblPrice";
-            this.lblPrice.Size = new System.Drawing.Size(31, 13);
-            this.lblPrice.TabIndex = 1;
-            this.lblPrice.Text = "Price";
-            // 
-            // lblUModel
-            // 
-            this.lblUModel.AutoSize = true;
-            this.lblUModel.Location = new System.Drawing.Point(24, 78);
-            this.lblUModel.Name = "lblUModel";
-            this.lblUModel.Size = new System.Drawing.Size(74, 13);
-            this.lblUModel.TabIndex = 3;
-            this.lblUModel.Text = "Update Model";
-            // 
-            // lblSearchId
-            // 
-            this.lblSearchId.AutoSize = true;
-            this.lblSearchId.Location = new System.Drawing.Point(24, 30);
-            this.lblSearchId.Name = "lblSearchId";
-            this.lblSearchId.Size = new System.Drawing.Size(55, 13);
-            this.lblSearchId.TabIndex = 2;
-            this.lblSearchId.Text = "Search ID";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(24, 104);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(69, 13);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Update Price";
-            // 
-            // btnSave
-            // 
-            this.btnSave.Location = new System.Drawing.Point(98, 119);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(75, 23);
-            this.btnSave.TabIndex = 2;
-            this.btnSave.Text = "SAVE";
-            this.btnSave.UseVisualStyleBackColor = true;
-            // 
-            // btnFind
-            // 
-            this.btnFind.Location = new System.Drawing.Point(117, 46);
-            this.btnFind.Name = "btnFind";
-            this.btnFind.Size = new System.Drawing.Size(75, 23);
-            this.btnFind.TabIndex = 3;
-            this.btnFind.Text = "FIND";
-            this.btnFind.UseVisualStyleBackColor = true;
-            // 
-            // btnUpdate
-            // 
-            this.btnUpdate.Location = new System.Drawing.Point(117, 131);
-            this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
-            this.btnUpdate.TabIndex = 5;
-            this.btnUpdate.Text = "UPDATE";
-            this.btnUpdate.UseVisualStyleBackColor = true;
-            // 
-            // txtModel
-            // 
-            this.txtModel.Location = new System.Drawing.Point(85, 31);
-            this.txtModel.Name = "txtModel";
-            this.txtModel.Size = new System.Drawing.Size(100, 20);
-            this.txtModel.TabIndex = 3;
-            // 
-            // txtPrice
-            // 
-            this.txtPrice.Location = new System.Drawing.Point(85, 83);
-            this.txtPrice.Name = "txtPrice";
-            this.txtPrice.Size = new System.Drawing.Size(100, 20);
-            this.txtPrice.TabIndex = 4;
             // 
             // textBox3
             // 
@@ -199,21 +162,58 @@
             this.txtUPrice.Size = new System.Drawing.Size(100, 20);
             this.txtUPrice.TabIndex = 7;
             // 
-            // textBox1
+            // btnUpdate
             // 
-            this.textBox1.Location = new System.Drawing.Point(85, 57);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 6;
+            this.btnUpdate.Location = new System.Drawing.Point(117, 131);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
             // 
-            // lblMarka
+            // btnFind
             // 
-            this.lblMarka.AutoSize = true;
-            this.lblMarka.Location = new System.Drawing.Point(20, 60);
-            this.lblMarka.Name = "lblMarka";
-            this.lblMarka.Size = new System.Drawing.Size(37, 13);
-            this.lblMarka.TabIndex = 5;
-            this.lblMarka.Text = "Marka";
+            this.btnFind.Location = new System.Drawing.Point(117, 46);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 3;
+            this.btnFind.Text = "FIND";
+            this.btnFind.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 104);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Update Price";
+            // 
+            // lblUModel
+            // 
+            this.lblUModel.AutoSize = true;
+            this.lblUModel.Location = new System.Drawing.Point(24, 78);
+            this.lblUModel.Name = "lblUModel";
+            this.lblUModel.Size = new System.Drawing.Size(74, 13);
+            this.lblUModel.TabIndex = 3;
+            this.lblUModel.Text = "Update Model";
+            // 
+            // lblSearchId
+            // 
+            this.lblSearchId.AutoSize = true;
+            this.lblSearchId.Location = new System.Drawing.Point(24, 30);
+            this.lblSearchId.Name = "lblSearchId";
+            this.lblSearchId.Size = new System.Drawing.Size(55, 13);
+            this.lblSearchId.TabIndex = 2;
+            this.lblSearchId.Text = "Search ID";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 223);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(467, 187);
+            this.dataGridView1.TabIndex = 2;
             // 
             // Form1
             // 
